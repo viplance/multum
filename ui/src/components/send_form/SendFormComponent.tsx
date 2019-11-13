35144d075f047e3e88c1982d0f7c6707b8ba767e
Dzmitry Sharko <viplance.com@gmail.com>
2019-11-13 16:36:29 +0300

    Preparing for sending data to store.

--- ui/src/components/send_form/SendFormComponent.tsx
+++ ui/src/components/send_form/SendFormComponent.tsx
@@ -5,15 +5,20 @@ import theme from '../../theme';
 /* Assets */
 import send from '../../assets/send.svg';
 
+
 const SendFormComponent = (props: any) => {
+  const sendMessage = () => {
+    const text = (document.getElementById('messageText') as HTMLTextAreaElement).value;
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <SendForm>
-        <textarea placeholder="Write your message here"></textarea>
-        <img src={send} alt="send" title="Send message" />
+        <textarea placeholder="Write your message here" id="messageText"></textarea>
+        <img src={send} alt="send" title="Send message" onClick={sendMessage} />
       </SendForm>
     </ThemeProvider>
-  )
-}
+  );
+};
 
 export default SendFormComponent;
