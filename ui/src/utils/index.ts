4969aab209864157f270669a16d989d0466ed744
Dzmitry Sharko <viplance.com@gmail.com>
2019-11-14 16:44:49 +0300

    Added local storage methods.

--- ui/src/utils/index.ts
+++ ui/src/utils/index.ts
@@ -1,3 +1,5 @@
+const LOCAL_STORAGE_NAME = 'multum';
+
 export function transformDate(date: Date = new Date()): string {
   const mm = date.getMonth() + 1;
   const dd = date.getDate();
@@ -6,4 +8,34 @@ export function transformDate(date: Date = new Date()): string {
   const sec = date.getSeconds();
 
   return `${(mm > 9 ? '' : '0') + mm}.${(dd > 9 ? '' : '0') + dd}.${date.getFullYear()}. ${hrs}:${min}:${sec}`;
+}
+
+export function getLocal(): any {
+  let lS: object;
+  try {
+    lS = JSON.parse(localStorage.getItem(LOCAL_STORAGE_NAME) as string);
+  } catch (e) {
+    lS = {};
+  }
+  if (lS === null) {
+    lS = {};
+  }
+  return lS;
+}
+
+export function removeLocal() {
+  localStorage.removeItem(LOCAL_STORAGE_NAME);
+}
+
+export function setLocal(obj: object) {
+  const lS = getLocal() as any;
+  let newLs = cloneObject(lS);
+  newLs = Object.assign(newLs, obj);
+  if (JSON.stringify(lS) !== JSON.stringify(newLs)) {
+    localStorage.setItem(LOCAL_STORAGE_NAME, JSON.stringify(newLs));
+  }
+}
+
+function cloneObject(obj: {}) {
+  return JSON.parse(JSON.stringify(obj));
 }
