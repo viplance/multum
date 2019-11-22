a22d37c13f0526e226a937b3ea2ae33205863baa
Dzmitry Sharko <viplance.com@gmail.com>
2019-11-22 17:29:23 +0300

    SigninCopmonent has been improved.

--- ui/src/containers/Signin/SigninContainer.tsx
+++ ui/src/containers/Signin/SigninContainer.tsx
@@ -1,17 +1,39 @@
-import React, { FC, ReactElement } from 'react';
+import React, { ChangeEvent, FC, FormEvent, ReactElement, useState } from 'react';
 /* Assets */
 import './SigninContainer.css';
 
 const SigninContainer: FC = (): ReactElement => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const target: any = event.nativeEvent.target;
+    const elements: HTMLFormControlsCollection = target.elements;
+    console.log(elements.length);
+    setUsername('');
+    setPassword('');
+  };
+
+  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setUsername(value);
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setPassword(value);
+  };
+
   return (
     <div className="Signin">
-      <form action="index.html" method="post">
+      <form onSubmit={handleSubmit}>
         <h1>Sign in</h1>
         <div className="form-content">
-          <input id="user-name" name="user-name" placeholder="user name" type="text" />
-          <input id="password" name="password" placeholder="password" type="password" />
+          <input value={username} required placeholder="user name" onChange={handleUsernameChange} type="text" />
+          <input value={password} required placeholder="password" onChange={handlePasswordChange} type="password" />
           <br />
-          <div className="button">Log in</div>
+          <input type="submit" className="button" value="Log in" />
           <br />
           <div className="signup-message">
             <a href="#">Forgot your password?</a>
